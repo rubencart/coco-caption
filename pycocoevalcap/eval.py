0a79339256addba31590631c315c6ebdcc0bd81d
rubencart <ruben.cartuyvels@gmail.com>
2019-04-03 19:47:52 +0000

    dont import spice, wmd

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -8,8 +8,8 @@ from .bleu.bleu import Bleu
 from .meteor.meteor import Meteor
 from .rouge.rouge import Rouge
 from .cider.cider import Cider
-from .spice.spice import Spice
-from .wmd.wmd import WMD
+# from .spice.spice import Spice
+# from .wmd.wmd import WMD
 
 
 class COCOEvalCap:
