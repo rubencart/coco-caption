d1037c593d2a3ed9aba63e197fa63f69fc7d37d8
Ruben <ruben.cartuyvels@gmail.com>
2019-05-05 21:52:11 +0200

    less logging

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -33,7 +33,7 @@ class COCOEvalCap:
         # =================================================
         # Set up scorers
         # =================================================
-        print('tokenization...')
+        # print('tokenization...')
         tokenizer = PTBTokenizer()
         gts = tokenizer.tokenize(gts)
         res = tokenizer.tokenize(res)
@@ -41,7 +41,7 @@ class COCOEvalCap:
         # =================================================
         # Set up scorers
         # =================================================
-        print('setting up scorers...')
+        # print('setting up scorers...')
         scorers = [
             (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
             # (Meteor(),"METEOR"),
@@ -55,17 +55,17 @@ class COCOEvalCap:
         # Compute scores
         # =================================================
         for scorer, method in scorers:
-            print('computing %s score...'%(scorer.method()))
+            # print('computing %s score...'%(scorer.method()))
             score, scores = scorer.compute_score(gts, res)
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
                     self.setImgToEvalImgs(scs, list(gts.keys()), m)
-                    print("%s: %0.3f"%(m, sc))
+                    # print("%s: %0.3f"%(m, sc))
             else:
                 self.setEval(score, method)
                 self.setImgToEvalImgs(scores, list(gts.keys()), method)
-                print("%s: %0.3f"%(method, score))
+                # print("%s: %0.3f"%(method, score))
         self.setEvalImgs()
 
     def setEval(self, score, method):
